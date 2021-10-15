941060392d0073da82805d58e3f223eb829f8cb3
xulzu <xulzu@qq.com>
2021-10-15 15:02:08 +0800

    feat:接入webpack

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './assets/firework.mp3';
+import './assets/发射.mp3';
+import './assets/发射2.mp3';
+import './assets/爆炸.mp3';
+import './index.less';
+import './index.js';
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const { resolve } = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+  mode: 'development', //通过配置是开发环境还是生产环境，webpack会有不同的优化策略
+  entry: {
+    index: './src/main.js',
+  },
+  output: {
+    filename: 'js/[index]-[hash:5].js',
+    path: resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[name][ext][query]',
+    clean: true
+  },
+  // webpack-dev-server need webpack-dev-server plugin
+  devServer: {
+    // 设置端口
+    static: './dist/',
+    port: 1300,
+    open: true,
+    hot: true
+  },
+  module: {
+    rules: [{
+      test: /\.(less|css)$/i,
+      use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader']
+    },
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'babel-loader',
+      },
+      ],
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif|mp3)$/i,
+      type: 'asset/resource',
+    },
+    {
+      test: /\.html$/i,
+      use: [{
+        loader: 'html-loader',
+      }]
+    }
+    ]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name]-[hash:5].css'
+    })
+  ],
+  resolve: {
+    // 配置可以忽略那些类型的文件后缀。
+    extensions: [
+      '.js', '.ts'
+    ],
+    modules: [
+      resolve(__dirname, './node_modules'), 'node_modules'
+    ]
+  }
+};
